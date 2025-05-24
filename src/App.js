fb427d7c283e5313fe0aacfea03e0e547290507e
yash <yashsaundalkar0@gmail.com>
2025-05-24 20:55:15 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/Signup';
-import Admin from './components/Admin/Admin'; // Example protected page
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/gym-management-system">
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/admin" element={<Admin />} />
-        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
