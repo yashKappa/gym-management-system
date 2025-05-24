cd46ee066dc8d2d0152fdbcf9302a20e7c9655ae
yash <yashsaundalkar0@gmail.com>
2025-05-24 21:00:48 +0530

    dfgd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/Signup';
 import Admin from './components/Admin/Admin';
 
 function App() {
   return (
-    <Router basename="/gym-management-system">
+     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
