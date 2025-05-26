54323f24f9bb822ea67c93df3e7e5a20078a57e7
yash <yashsaundalkar0@gmail.com>
2025-05-26 20:15:22 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 // App.js
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { auth } from './components/Firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import Cookies from 'js-cookie';
+
 import Login from './components/Login/Login';
 import SignUp from './components/Login/Signup';
 import Admin from './components/Admin/Admin';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        Cookies.set('session', user.uid, { expires: 7 });
+        navigate('/admin');
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
